1afeaf49dc271f6c4d38b5b5f17b1cc450ad22b3
fahrezialwi <fahrezialwi@gmail.com>
2019-10-04 19:54:23 +0700

    October 4, 2019 course

--- search-filter-api/index.js
+++ search-filter-api/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var cors = require('cors')
-const db = require('./database/index')
+const db = require('./database')
 const port = 1912
 
 app.use(bodyParser.json())
@@ -13,7 +13,52 @@ app.get('/', (req, res) => {
 })
 
 app.get('/passengers', (req, res) => {
-    db.query(`select * from train`, (err, result) => {
+    let sql = `select * from train`
+    if (req.query){
+        sql = `${sql} where`
+
+        if(req.query.name){
+            sql = `${sql} Name like '%${req.query.name}%' and`
+        }
+        if(req.query.age_min){
+            sql = `${sql} Age >= ${req.query.age_min} and`
+        }
+        if(req.query.age_max){
+            sql = `${sql} Age <= ${req.query.age_max} and`
+        }
+        if(req.query.gender){
+            if (req.query.gender == 'all'){
+                sql = `${sql}`
+            } else {
+                sql = `${sql} Sex = '${req.query.gender}' and`
+            }
+        }
+        if(req.query.class){
+            if (req.query.class == 'all'){
+                sql = `${sql}`
+            } else {
+                sql = `${sql} Pclass = ${req.query.class} and`
+            }
+        }
+        if(req.query.survived){
+            if (req.query.survived == 'all'){
+                sql = `${sql}`
+            } else {
+                sql = `${sql} Survived = ${req.query.survived} and`
+            }
+        }
+        sql = sql.slice(0, -4)
+    }
+
+    db.query(sql, (err, result) => {
+        if(err) throw err
+        res.send(result)
+    })
+})
+
+app.get('/classes', (req, res) => {
+    let sql = `select Pclass from train group by Pclass order by Pclass`
+    db.query(sql, (err, result) => {
         if(err) throw err
         res.send(result)
     })
--- search-filter-app/src/components/Filter.jsx
+++ search-filter-app/src/components/Filter.jsx
@@ -1,28 +1,76 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-
+import querystring from 'query-string'
 const URL_API = 'http://localhost:1912/'
 
 class Filter extends Component {
     constructor(props){
         super(props)
         this.state = {
-            passengers: []
+            passengers: [],
+            name: '',
+            ageMin: '',
+            ageMax: '',
+            gender: '',
+            class: '',
+            survived: '',
+            classes: []
         }
     }
 
     componentDidMount () {
         this.getData()
+        this.getClassData()
     }
 
     getData = () => {
-        axios.get(URL_API + 'passengers')
-        .then(res => {
+        let params = querystring.parse(this.props.location.search)
+
+        axios.get(
+            URL_API + 'passengers', {
+                params: params
+            }
+        ).then(res => {
             this.setState({
-                passengers : res.data
+                passengers: res.data
             })
+        }).catch(err => {
+            console.log(err)
         })
-        .catch(err => {
+    }
+
+    onFilterButton = () => {
+        let params = {}
+        if(this.state.name){
+            params = {...params, name: this.state.name}
+        }
+        if(this.state.ageMin){
+            params = {...params, age_min: this.state.ageMin}
+        }
+        if(this.state.ageMax){
+            params = {...params, age_max: this.state.ageMax}
+        }
+        if(this.state.gender){
+            params = {...params, gender: this.state.gender}
+        }
+        if(this.state.class){
+            params = {...params, class: this.state.class}
+        }
+        if(this.state.survived){
+            params = {...params, survived: this.state.survived}
+        }
+
+        this.props.history.push(`/filter?${querystring.stringify(params)}`)
+
+        axios.get(
+            URL_API + 'passengers', {
+                params: params
+            }
+        ).then(res => {
+            this.setState({
+                passengers: res.data
+            })
+        }).catch(err => {
             console.log(err)
         })
     }
@@ -33,11 +81,31 @@ class Filter extends Component {
                 <tr key={index}>
                     <td>{passenger.PassengerId}</td>
                     <td>{passenger.Name}</td>
-                    <td>{passenger.Sex === "male" ? "M" : "F"}</td>
                     <td>{passenger.Age}</td>
-                    <td>{passenger.Pclass === 1 ? "First Class" : passenger.Pclass === 2 ? "Business" : "Economy"}</td>
+                    <td>{passenger.Sex === "male" ? "Male" : "Female"}</td>
+                    <td>{passenger.Pclass}</td>
                     <td>{passenger.Survived ? "Alive" : "Deceased"}</td>
                 </tr>
+            )
+        })
+    }
+
+    getClassData = () => {
+        axios.get(
+            URL_API + 'classes'
+        ).then(res => {
+            this.setState({
+                classes: res.data
+            })
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    classList = () => {
+        return this.state.classes.map((val, index) => {
+            return (
+                <option key={index} value={val.Pclass}>{val.Pclass}</option>
             )
         })
     }
@@ -45,14 +113,59 @@ class Filter extends Component {
     render() {
         return (
             <div className="container">
+                <div className="row mt-5 mb-4">
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <h6>Name</h6>
+                        <input ref="name" onChange={e => this.setState({name: e.target.value})} className="form-control"/>
+                    </div>
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <h6>Age</h6>
+                        <div className="row">
+                            <div className="col-6 pr-1">
+                                <input ref="ageMin" onChange={e => this.setState({ageMin: e.target.value})} className="form-control" placeholder="Min"/>
+                            </div>
+                            <div className="col-6 pl-1">  
+                                <input ref="ageMax" onChange={e => this.setState({ageMax: e.target.value})} className="form-control" placeholder="Max"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <h6>Gender</h6>
+                        <select onChange={e => this.setState({gender: e.target.value})} className="form-control">
+                            <option value="all">All</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <h6>Class</h6>
+                        <select onChange={e => this.setState({class: e.target.value})} className="form-control">
+                            <option value="all">All</option>
+                            {this.classList()}
+                        </select>
+                    </div>
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <h6>Survived</h6>
+                        <select onChange={e => this.setState({survived: e.target.value})} className="form-control">
+                            <option value="all">All</option>
+                            <option value="1">Alive</option>
+                            <option value="0">Deceased</option>
+                        </select>
+                    </div>
+                    <div className="col-lg-2 col-md-4 col-sm-4 mb-3">
+                        <button onClick={() => this.onFilterButton()} className="btn btn-primary btn-block" style={{marginTop: "27px"}}>Filter</button>
+                    </div>
+                </div>
                 <table className="table">
                     <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Gender</th>
-                        <th>Age</th>
-                        <th>Class</th>
-                        <th>Survived</th>
+                        <tr>
+                            <th width="8%">ID</th>
+                            <th width="52%">Name</th>
+                            <th width="10%">Age</th>
+                            <th width="10%">Gender</th>
+                            <th width="10%">Class</th>
+                            <th width="10%">Survived</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {this.passengerList()}
