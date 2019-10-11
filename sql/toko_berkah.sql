d15d9c5c92f1ad7ce66aab1562f2e08bb3203066
fahrezialwi <fahrezialwi@gmail.com>
2019-10-11 15:11:50 +0700

    October 11, 2019 course

--- sql/toko_berkah.sql
+++ sql/toko_berkah.sql
@@ -28,3 +28,17 @@ join kota as k on t.kotaId = k.id
 where k.nama in ('Jakarta','Tangerang','Bandung'))
 group by k.nama
 order by jumlah_toko desc;
+
+select t.id as id_toko, t.nama as nama_toko, t.alamat,
+t.totalIncome as total_income, t.tanggalBerdiri as tanggal_berdiri,
+k.id as id_kota, k.nama as nama_kota, 
+extract(year from tanggalBerdiri) as tahun,
+extract(month from tanggalBerdiri) as bulan,
+extract(day from tanggalBerdiri) as tanggal
+from toko as t join kota as k on t.kotaId = k.id
+where t.tanggalBerdiri between '2017-12-01' and '2018-12-01'
+and k.nama in ('Jakarta','Tangerang')
+and t.totalIncome > 
+(select avg(t.totalIncome) from toko as t 
+join kota as k on t.kotaId = k.id 
+where k.nama = 'Bandung');
